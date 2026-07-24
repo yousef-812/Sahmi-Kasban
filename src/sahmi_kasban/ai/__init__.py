a2da3929d216e4d906f846de899734fcb59a7ea7
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-25 01:26:08 +0300

    Export AI integration

---
+++ src/sahmi_kasban/ai/__init__.py
@@ -0,0 +1,9 @@
+from sahmi_kasban.ai.client import AIChatClient, AIClientConfig, AIProviderError
+from sahmi_kasban.ai.service import SahmiAIService
+
+__all__ = [
+    "AIChatClient",
+    "AIClientConfig",
+    "AIProviderError",
+    "SahmiAIService",
+]
