a5431da013688055dd4009c70fb586dee3668db5
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-25 02:08:45 +0300

    Export foundational models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,21 @@
+from app.models.entities import (
+    Discussion,
+    MarketReport,
+    MarketReportItem,
+    PredictionVerification,
+    StockAnalysis,
+    Subscription,
+    User,
+    WalletEntry,
+)
+
+__all__ = [
+    "Discussion",
+    "MarketReport",
+    "MarketReportItem",
+    "PredictionVerification",
+    "StockAnalysis",
+    "Subscription",
+    "User",
+    "WalletEntry",
+]
