90556c768f46df65e1cec477972f82241036a177
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-26 20:08:50 +0300

    Format Flutter observability test

--- mobile/test/app_observability_test.dart
+++ mobile/test/app_observability_test.dart
@@ -21,9 +21,6 @@ void main() {
 
     expect(find.textContaining('حدث خطأ غير متوقع'), findsOneWidget);
     final semantics = tester.getSemantics(find.byType(Semantics).last);
-    expect(
-      semantics.label,
-      contains('حدث خطأ غير متوقع'),
-    );
+    expect(semantics.label, contains('حدث خطأ غير متوقع'));
   });
 }
