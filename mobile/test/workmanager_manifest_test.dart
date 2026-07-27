2e9fdd0107c8f94d0ce5776bd31d0d34554cc8fd
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-27 20:12:50 +0300

    Add regression test for Android startup manifest

---
+++ mobile/test/workmanager_manifest_test.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Android manifest disables eager WorkManager startup', () {
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
+
+    expect(manifest, contains('xmlns:tools="http://schemas.android.com/tools"'));
+    expect(manifest, contains('androidx.startup.InitializationProvider'));
+    expect(manifest, contains('androidx.work.WorkManagerInitializer'));
+    expect(manifest, contains('tools:node="remove"'));
+  });
+}
